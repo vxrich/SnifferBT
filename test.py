44725ad9876b11f6f4ccdaf670cc0abd784dcc2f
Riccardo Grespan <riccardo.grespan6@gmail.com>
2018-08-06 20:29:23 +0200

    Indentation

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ print "found %d devices" % len(nearby_devices)
 for addr, name in nearby_devices:
     print " %s - %s" % (addr, name)
     for services in bluetooth.find_service(address = addr):
-    print " Name: %s" % (services["name"])
-    print " Description: %s" % (services["description"])
-    print " Protocol: %s" % (services["protocol"])
-    print " Provider: %s" % (services["provider"])
-    print " Port: %s" % (services["port"])
-    print " Service id: %s" % (services["service-id"])
-    print ""
-    print ""
+	print " Name: %s" % (services["name"])
+    	print " Description: %s" % (services["description"])
+    	print " Protocol: %s" % (services["protocol"])
+    	print " Provider: %s" % (services["provider"])
+    	print " Port: %s" % (services["port"])
+    	print " Service id: %s" % (services["service-id"])
+    	print ""
+    	print ""
