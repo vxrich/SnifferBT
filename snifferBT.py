cbb5e2bd5ec148a20117df46ee5bf7c3a7de1c38
Riccardp Grespan <vxrich@localhost.localdomain>
2018-08-10 23:51:05 +0200

    Made progress with SnifferBT.py, add TODO.txt file

--- snifferBT.py
+++ snifferBT.py
@@ -8,13 +8,56 @@ Il tutto per stimare la numero di persone presenti in un ambiente.
 
 '''
 
-from bluetooth.ble import DiscoveryService #Modulo per scanning dei device
-from bluetooth.ble import BeaconService #Modulo per effettuare advertising da parte del Raspberry 
+#Questo codice trova il telefono sia da RPi sia da MiNotebook (Funziona una volta poi non funziona più)
+
+from bluetooth import bluez #Modulo base per l'interazione bluetooth 
+from bluetooth import ble #Modulo per lo scanning BLE
+import MySQLdb #Modulo interazione DB dei dispositivi scansionati
 import time
 
-serviceDS = DiscoveryService()
-serviceBS = BeaconService()
-devices = serviceDS.discover(20)
+WAITING_TIME = 120 #Secondi
 
-for address, name in devices.items():
-    print("name: {}, address: {}".format(name, address))
+HOSTNAME = ""
+ID = ""
+PSW = ""
+DB_NAME = ""
+
+LOAD_QUERY = ""
+
+db = MySQLdb.connect(HOST, ID, PSW, DB_NAME)
+cur = db.cursor()
+
+
+def scan_devices():
+
+    print "Start scanning devices ..."
+    
+    devices = bluez.discover_devices(duration=2, flush_cache=True, lookup_names=True)
+    #devices = ble.discover_devices()
+
+    print "Printing Data ..."
+
+    return devices
+
+def print_devices(devices):
+
+    for address, name in devices.items():
+        print("name: {}, address: {}".format(name, address))
+
+def load_data():
+    
+    cur.execute(LOAD_QUERY)
+
+
+while True:
+
+    scan_devices()
+
+    load_data()
+    
+
+
+
+
+
+
