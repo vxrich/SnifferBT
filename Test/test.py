7f8013b6f1c7b566e2031783e02c2383060e2922
Riccardp Grespan <vxrich@localhost.localdomain>
2018-08-13 22:29:40 +0200

    Removed bleep

--- Test/test.py
+++ Test/test.py
@@ -1,6 +1,6 @@
 from bluetooth import bluez
 from bluetooth import ble 
-from bleep import BLEDevice
+from bluetooth import *
 import datetime
 
 print "Debug"
@@ -8,9 +8,8 @@ print "Start scanning devices ..."
 
 while True:
         print datetime.datetime.now()   
-        #devices = bluez.discover_devices(duration=2, flush_cache=True, lookup_names=True, device_id=0)
+        devices = bluez.discover_devices(duration=2, flush_cache=True, lookup_names=True, device_id=0)
         #devices = ble.discover_devices()
-        devices = BLEDevice.discoverDevices(device="hci0", timeout=3)
 
 
         for device in devices:
