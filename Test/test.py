95aeea94a4efffb9b332b7e86a47e7eb1a28f4da
Riccardp Grespan <vxrich@localhost.localdomain>
2018-08-13 12:01:36 +0200

    Import bleep module to test the ble scan

--- Test/test.py
+++ Test/test.py
@@ -1,17 +1,24 @@
 from bluetooth import bluez
 from bluetooth import ble 
-from bluetooth import btcommon
+from bluetooth import 
+from bleep import BLEDevice
+import bluepy
+import datetime
 
 print "Debug"
 print "Start scanning devices ..."
 
+while True:
+        print datetime.datetime.now()   
+        #devices = bluez.discover_devices(duration=2, flush_cache=True, lookup_names=True, device_id=0)
+        #devices = ble.discover_devices()
+        devices = BLEDevice.discoverDevices()
 
-devices = bluez.discover_devices(duration=2, flush_cache=True, lookup_names=True, device_id=0)
-#devices = ble.discover_devices()
 
+        for device in devices:
+                print device
 
-for device in devices:
-	print device
+        print "--------------------------------------------------------------------"
 
 #for address, name in devices.items():
 #        print("name: {}, address: {}".format(name, address))
