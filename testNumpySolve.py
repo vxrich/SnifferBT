fd93e333ca026a6f89b30b4e86331d5a38a4863a
Riccardo Grespan <vxrich@localhost.localdomain>
2018-12-14 11:03:21 +0100

    Checking traingulation function, VSC terminal not working

--- testNumpySolve.py
+++ testNumpySolve.py
@@ -1,17 +1,32 @@
 import numpy as np 
-from numpy.linalg import solve
+from numpy.linalg import det
+import scipy as sp 
 
-import numpy as np
 
 """
 Circonferenza alfa : (x-2)^2 + (y-4)^2 = 4
 Circonferenza beta : x^2 + (y-2)^2 =4
-Circonferenza gamma : (x-3)^2 + (y-3)^2 = 4
+Circonferenza gamma : (x-3)^2 + (y-2)^2 = 4
 """
 
-alfa = np.array([-2,-4,16])
-beta = np.array([0,-2,0])
-gamma = np.array([-3,-1,16])
+def deter(A):
+
+    return A[0][0]*A[1][1]-A[0][1]*A[1][0]
+
+def cramer(A,b):
+    det = deter(A)
+
+    Ax = [b,A[1]]
+    x = deter(Ax)/det
+
+    Ay=[A[0],b]
+    y = deter(Ay)/det
+
+    return x,y
+
+alfa = np.array([-4,-8,16])
+beta = np.array([0,-4,0])
+gamma = np.array([-6,-4,16])
 
 eq=np.subtract(alfa,beta)
 eq1=np.subtract(beta,gamma)
@@ -21,4 +36,8 @@ b = np.array([eq[2], eq1[2]])
 
 print A
 print b
-x = np.linalg.solve(A,b)
+
+x=np.linalg.solve(A,b)
+
+x,y = cramer(A,b)
+print x,y
