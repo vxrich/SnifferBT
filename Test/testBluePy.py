2d469a69f29ec47c335d7604729b6c66d11c2514
Riccardo Grespan <vxrich@localhost.localdomain>
2019-01-18 14:04:04 +0100

    Minor correction

--- Test/testBluePy.py
+++ Test/testBluePy.py
@@ -31,4 +31,5 @@ devices = scanner.scan(10)
 for dev in devices:
     print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
     for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
+        print "  %s = %s\n" % (desc, value)
+        
