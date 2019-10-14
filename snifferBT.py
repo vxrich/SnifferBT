5019f0f402bb59212fc2c5835b03a942bf2dcbc1
vxrich <vxrich@localhost.localdomain>
2019-10-14 17:30:17 +0200

    Update

--- snifferBT.py
+++ snifferBT.py
@@ -15,8 +15,8 @@ import os
 import time
 
 #from bluetooth.ble import BeaconService
-from bluepy.btle import Scanner
-from bluetooth import bluez
+#from bluepy.btle import Scanner
+#from bluetooth import bluez
 import MySQLdb #Modulo interazione DB
 import cPickle as pickle
 from GATTServices import GATTServices
